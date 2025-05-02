9ebe18548c9c6e705cc49ddb900328f7fd775317
Onurcan Okay <onurcanokay@hotmail.com>
2025-05-02 11:56:55 +0300

    Update task2.py

--- local/task2.py
+++ local/task2.py
@@ -3,6 +3,7 @@ import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
+import cv2
 
 if __name__ == "__main__":
     map_path = r"C:\Users\Onur Can\Desktop\mechademy\mechademy\semantic_map_00800-TEEsavR23oF.pkl"
@@ -10,45 +11,26 @@ if __name__ == "__main__":
         semantic_map = pickle.load(f)
     semantic_map.refine_semantic_predictions()
 
-    #print(type(semantic_map.rgb_observations))
-    #print(type(semantic_map.semantic_observations))
-    #print(type(semantic_map.semantic_prediction_masks))
-    #print(semantic_map.instance_id_to_name)
-    #for gt_mask_id in semantic_map.instance_id_to_name:
-    #    print(gt_mask_id)
-
-    #semantic_map.display_rgb_observation(1)
-    #semantic_map.display_semantic_observation(1)
-    
-    #semantic_obs = semantic_map.semantic_prediction_masks.get(1, None)
-    #num_classes = len(semantic_map.get_semantic_class_names())
-    #cmap = ListedColormap(np.random.rand(num_classes, 3))
-    #plt.figure(figsize=(6, 12))
-    #plt.imshow(semantic_obs, cmap=cmap)
-    #plt.axis('off')
-    #plt.show()
-
-
-    #gt_mask = semantic_map.semantic_observations[1].copy()
-    #gt_object_mask = gt_mask == 767
-    #plt.figure(figsize=(6, 12))
-    #plt.imshow(gt_object_mask)
-    #plt.axis('off')
-    #plt.show()
-    #print(gt_object_mask.shape)
-    #print(gt_object_mask[128][30])
-    
-
-    im = 0
     try:
+        img_id = 0
         while True:
             try:
-                label_id = 0
+                mask_id = 0
                 while True:
-                    most_common = semantic_map.match_predicted_object_with_gt(im,label_id)
-                    int_over_un = semantic_map.match_predicted_object_with_gt_IoU(im,label_id)
+                    most_common = semantic_map.match_predicted_object_with_gt(img_id,mask_id)
+                    int_over_un = semantic_map.match_predicted_object_with_gt_IoU(img_id,mask_id)
                     if most_common != int_over_un:
-                        print(f"For image {im}, two alternatives differ\nMost common ({most_common})\nInter over union ({int_over_un})")
+                        print(f"For image {im}, two alternatives differ\nMost common {most_common}\nInter over union {int_over_un}\n")
+
+                        rgb_img = semantic_map.rgb_observations[img_id].copy()
+                        mask = semantic_map.refined_prediction_masks[img_id][mask_id]
+                        kernel = np.ones((3, 3), np.uint8)
+                        dilated_mask = cv2.dilate(mask,kernel,iterations=1)
+                        edges = dilated_mask - mask
+                        rgb_img[edges==1] = [255,0,0]
+                        
+                        plt.figure(figsize=(6, 12))
+                        plt.imshow()
                     label_id = label_id + 1
             except KeyError:
                 pass
